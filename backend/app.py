853ff7045939986e9c824e71167203a253ede89c
pankajapatel26 <34297513+pankajapatel26@users.noreply.github.com>
2026-08-02 08:01:30 +0530

    Add backend/app.py

---
+++ backend/app.py
@@ -0,0 +1,88 @@
+# Import necessary libraries
+import numpy as np
+import joblib  # For loading the serialized model
+import pandas as pd  # For data manipulation
+from flask import Flask, request, jsonify  # For creating the Flask API
+
+# Initialize the Flask application
+superkart_sales_predictor_api = Flask("SuperKart Predictor")
+
+# Load the trained machine learning model
+model = joblib.load("SuperKart_prediction_model_v1_0.joblib")
+
+# Define a route for the home page (GET request)
+@superkart_sales_predictor_api.get('/')
+def home():
+    """
+    This function handles GET requests to the root URL ('/') of the API.
+    It returns a simple welcome message.
+    """
+    return "Welcome to the SuperKart Sales Prediction API!"
+
+# Define an endpoint for single property prediction (POST request)
+@superkart_sales_predictor_api.post('/v1/predict')
+def predict_superkart_sales():
+    """
+    This function handles POST requests to the '/v1/predict' endpoint.
+    It expects a JSON payload containing property details and returns
+    the predicted superkart sales as a JSON response.
+    """
+    # Get the JSON data from the request body
+    property_data = request.get_json()
+
+    # Extract relevant features from the JSON data
+    sample = {
+        'Product_Weight': property_data['Product_Weight'],
+        'Product_Sugar_Content': property_data['Product_Sugar_Content'],
+        'Product_Allocated_Area': property_data['Product_Allocated_Area'],
+        'Product_Type_Category': property_data['Product_Type_Category'],
+        'Product_MRP': property_data['Product_MRP'],
+        'Store_Size': property_data['Store_Size'],
+        'Store_Location_City_Type': property_data['Store_Location_City_Type'],
+        'Store_Type': property_data['Store_Type'] ,
+        'Store_Age_Years': property_data['Store_Age_Years'   
+    }
+
+
+    # Convert the extracted data into a Pandas DataFrame
+    input_data = pd.DataFrame([sample])
+
+    # Make prediction (get log_price)
+    predicted_product_sales = model.predict(input_data)[0]
+
+    predicted_sales = round(float(predicted_product_sales), 2)
+
+    # Return the actual price
+    return jsonify({'Predicted Sales ': predicted_sales})
+
+
+# Define an endpoint for batch prediction (POST request)
+@superkart_sales_predictor_api.post('/v1/predictbatch')
+def predict_superkart_sales_batch():
+    """
+    This function handles POST requests to the '/v1/predictbatch' endpoint.
+    It expects a CSV file containing property details for multiple properties
+    and returns the predicted rental prices as a dictionary in the JSON response.
+    """
+    # Get the uploaded CSV file from the request
+    file = request.files['file']
+
+    # Read the CSV file into a Pandas DataFrame
+    input_data = pd.read_csv(file)
+
+    # Make predictions for all properties in the DataFrame (get Product_Store_Sales_Total)
+    predicted_product_sales = model.predict(input_data).tolist()
+
+    # Calculate actual prices
+    predicted_sales = [Product_Store_Sales_Total for Product_Store_Sales_Total in predicted_product_sales]
+
+    # Create a dictionary of predictions with property IDs as keys
+    property_ids = input_data['Product_Id_char'].tolist()  # Assuming 'id' is the property ID column
+    output_dict = dict(zip(property_ids, predicted_sales))  # Use actual prices
+
+    # Return the predictions dictionary as a JSON response
+    return output_dict
+
+# Run the Flask application in debug mode if this script is executed directly
+if __name__ == '__main__':
+    superkart_sales_predictor_api.run(debug=True)
